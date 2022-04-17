3daa07a668f9e865c62f7e4fae3beb6ea1602a4c
HyeJung Go <gpwjd0919@sookmyung.ac.kr>
2022-04-17 19:50:45 +0900

    북마크 스크롤까지 완료

---
+++ bookmark_final/src/main/java/com/example/bookmark/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.bookmark
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    val FoodList = arrayListOf<Food>(
+        Food("tteokbokki", "떡볶이", "30min"),
+        Food("braised", "찜닭", "60min"),
+        Food("braised", "찜닭", "60min"),
+        Food("braised", "찜닭", "60min"),
+        Food("braised", "찜닭", "60min"),
+        Food("braised", "찜닭", "60min"),
+        Food("braised", "찜닭", "60min"),
+        Food("braised", "찜닭", "60min"),
+        Food("braised", "찜닭", "60min"),
+        Food("braised", "찜닭", "60min"),
+        Food("braised", "찜닭", "60min"),
+        Food("braised", "찜닭", "60min"),
+        Food("braised", "찜닭", "60min"),
+        Food("braised", "찜닭", "60min")
+
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val aAdapter = Adapter(this, FoodList)
+        recyclerView.adapter = aAdapter
+
+        val gm = GridLayoutManager(this, 3)
+        recyclerView.layoutManager = gm
+        recyclerView.setHasFixedSize(true)
+
+    }
+}
