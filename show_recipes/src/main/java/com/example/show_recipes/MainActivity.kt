e2620daedd384ec1591e6d0f268e9a9b5db2cb7f
nabi4442 <nabi4442@naver.com>
2022-04-23 00:24:08 +0900

    [민정이 데이터 읽기 완료, 언니껀 아직,,/50프로if문 통과/이미지를 띄어올 수 없음]

--- show_recipes/src/main/java/com/example/show_recipes/MainActivity.kt
+++ show_recipes/src/main/java/com/example/show_recipes/MainActivity.kt
@@ -12,13 +12,13 @@ class MainActivity : AppCompatActivity() {
 
     val db = FirebaseFirestore.getInstance() //Firestore 인스턴스 선언
 
-    val recipes_List = arrayListOf<menu>(    //리사이클러뷰에 들어갈 메뉴 클래스 recipes_List
+    var recipes_List = arrayListOf<menu>(    //리사이클러뷰에 들어갈 메뉴 클래스 recipes_List
     )
 
     lateinit var recyclerView_recipes_list : RecyclerView //메뉴 리사이클러뷰(가로 슬라이드)
 
-    val user_refrigerator = arrayListOf<String>() //괄호 안에 냉장고 재료 받기
-    val ingredients = mutableListOf<String>("돼지고기" , "돼지 볼살", "돼기고기", "청포묵")
+    var user_refrigerator = arrayListOf<String>() //괄호 안에 냉장고 재료 받기
+    val ingredients = mutableListOf<String>("국수" , "소면", "사과", "청포묵")
 
 //    val ingredients1 = mutableListOf<String>("쌀" , "안심", "콩나물", "청포묵", "미나리", "소금")
 //    val ingredients2 = mutableListOf<String>("찹쌀" , "수수", "차조", "소금", "콩", "팥")
@@ -40,37 +40,57 @@ class MainActivity : AppCompatActivity() {
         recyclerView_recipes_list.layoutManager = recipes_linear_manager
         recyclerView_recipes_list.setHasFixedSize(true)
 
-        //Firestore "UserSelect"에서 냉장고 데이터 user_Refrigerator 변수로 받아오기
+
+        firestore_data_read() //데이터 읽고 메뉴 1차 필터링
+    }
+
+
+    private fun firestore_data_read(){
         db.collection("UserSelect") //작업할 컬렉션
             .get() //문서 가져오기
             .addOnSuccessListener { result ->
-
                 //성공할 경우
                 user_refrigerator.clear()
                 for (document in result){ //가져온 문서들은 result에 들어감
-                    //Log.w("아이템", "${document.data["menuname"]}") menuname만 읽어들어짐!!
-                    val user_ingredients = document.data["menuname"].toString()
-                    user_refrigerator.add(user_ingredients)
+                    //Log.w("아이템", "${document.data["menuname"]}") //menuname만 읽어들어짐!!
+                    val user_ingredients = document.data["menuname"] as String
+                    // println("user_ingredients : $user_ingredients") //user_ingredients 입력 잘 받아지는 거 확인,,!
+                    user_refrigerator = user_refrigerator.plus(user_ingredients) as ArrayList<String>
+                    //println("user_refrigerator : $user_refrigerator") //user_refrigerator 입력 잘 받아지는 거 확인,,!
                 }
-            }
+                filter_menu(user_refrigerator, ingredients) //메뉴 1차 필터링
+            } //success 바깥으로 나올 경우 user_refrigerator 안에 받아놓은 재료들이 다 사라져서 함수를 사용 못함 -> 위 문장에서 함수 호출함!
             .addOnFailureListener{exception ->
                 //실패할 경우
                 Log.w("MainActivity_Error", "Error getting documents: $exception")
             }
-        //////////////////////////////////////////////////////////////////////////
 
-        filtermenu(this.user_refrigerator, ingredients)
     }
 
-    private fun filtermenu(user_refrigerator: ArrayList<String>, ingredients:MutableList<String>) {
 
-        //while문으로 레시피 처음부터 끝까지 훑기
+    private fun filter_menu(input_user_refrigerator: ArrayList<String>, input_ingredients:MutableList<String>) {
 
-        val available_materials = user_refrigerator.filter{it in ingredients}
+//        println("****************함수 들어왔당*****************")
+//        println("input_user_refrigerator : $input_user_refrigerator")
+//        println("input_ingredients : $input_ingredients")
+
+        //Algorithm. while문으로 레시피 처음부터 끝까지 훑기
+
+        //Problem. available_materials 변수에 user_refrigerator가 안 받아와짐 -> db문 안에 넣어서 해결 완료
+        val available_materials = input_user_refrigerator.filter{it in ingredients}
 //        val available_menu_id :String //id에 맞는 레시피 이미지, 이름, 시간 "recipe_information"파일에서 찾기
 
-        if (available_materials.size >= ingredients.size/2){
-            recipes_List.add(menu("bibimbap", "비빔밥","1시간"))
+        println("available_materials : $available_materials")
+        println("available_materials.size : ${available_materials.size}")
+        println("input_ingredients : ${input_ingredients.size}")
+        println("input_ingredients : ${input_ingredients.size/2}")
+
+        if (available_materials.size >= input_ingredients.size/2){
+            recipes_List = recipes_List.plus(menu("bibimbap", "비빔밥","1시간")) as ArrayList<menu>
+            //recipes_List.add(menu("bibimbap", "비빔밥","1시간"))
+
+            //Problem. recipes_List : [com.example.show_recipes.menu@78efa3b] 이런 식으로 출력이 된다,, 왜지,,ㅠ
+            println("recipes_List : $recipes_List")
         }
     }
 //        val available_materials1 = user_refrigerator.filter{it in ingredients1}
