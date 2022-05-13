2b083210dffba2f298fa032a20d84ddd9796dc2e
HyeJung Go <gpwjd0919@sookmyung.ac.kr>
2022-05-13 18:07:03 +0900

    0513 레시피 샘플데이터 받기 완료/화면꺼짐방지 추가

--- recipe_dt/src/main/java/com/example/recipe_dt/RecipeActivity.kt
+++ recipe_dt/src/main/java/com/example/recipe_dt/RecipeActivity.kt
@@ -1,32 +1,33 @@
 package com.example.recipe_dt
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.recipe_main.*
 
 class RecipeActivity : AppCompatActivity() {
-    val explainlist = arrayListOf<Recipe>(
-        Recipe("braised", "ㅇ아아아아아아 첫번째"),
-        Recipe("tteokbokki", "ㅇ아아아아아아 두번째"),
-        Recipe("jajangmyeon", "ㅇ아아아아아아 세번째"),
-        Recipe("null", "ㅇ아아아아아아 네번째"),
+    val explainlist = arrayListOf<Recipe>() //상세레시피 사진,설명 리스트
 
-        )
+
 
 
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.recipe_main)
 
+        //화면꺼짐방지
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
 //        val backBtn = findViewById<Button>(R.id.moveBack)
 //        //버튼 클릭시 상세페이지에서 다시 원래 화면으로 돌아감
 //        backBtn.setOnClickListener {
 //            finish()
 //        }
-
 
         val rAdapter = RecipeAdapter(this, explainlist)
         recyclerView_explain.adapter = rAdapter
@@ -34,6 +35,35 @@ class RecipeActivity : AppCompatActivity() {
         val gm = GridLayoutManager(this, 1)
         recyclerView_explain.layoutManager = gm
         recyclerView_explain.setHasFixedSize(true)
+
+
+
+        ////////////////// SQLite 데이터 받기
+        val db_sqlite = DataBaseHelper(this).readableDatabase //sql 인스턴스 선언
+
+        // 1차 추천으로 나온 메뉴 + 해당 메뉴 상세레시피 (RECIPE_ID로 예시 몇개 뽑아서 테스트 중)
+        val filtermenu = "select RECIPE_ID, COOKING_DC from recipe_description where RECIPE_ID in ('9');"
+
+
+//        "select RECIPE_ID, COOKING_DC from recipe_description where RECIPE_ID in ('4', '9');"
+
+        val cursor = db_sqlite.rawQuery(filtermenu, null)
+        cursor.run{
+            while(moveToNext()) {
+                val RECIPE_ID = cursor.getString(0)
+                val COOKING_DC = cursor.getString(1)
+                Log.w("레시피_RECIPE_ID : ", RECIPE_ID)
+                Log.w("레시피_COOKING_DC : ", COOKING_DC)
+
+                val available_explain = Recipe("tteokbokki", COOKING_DC, RECIPE_ID)
+                explainlist.add(available_explain)
+                rAdapter.notifyDataSetChanged() //리사이클러뷰 갱신
+            }
+        }
+
+
+
+
 
     }
 
