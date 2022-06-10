13936d6ffeb8ab1e1ebee1ca5c4dff6b85c79c7d
min922 <mjmj9999@naver.com>
2022-06-10 18:10:19 +0900

    민정 모듈연결

---
+++ 모듈연결용/home_dp/src/main/java/com/example/home_dp/RefMainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.home_dp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.home_dp.R.layout.activity_main_ref
+import com.example.main_dp.SelectMainActivity
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.Tab
+import kotlinx.android.synthetic.main.activity_main_ref.*
+import com.example.home_dp.R.id.tabContent as refTabcontent
+import kotlinx.android.synthetic.main.activity_main_ref.tabs
+
+
+class RefMainActivity : AppCompatActivity() {
+    private lateinit var refTab: RefTab
+    private lateinit var bookmarkTab: BookmarkTab
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_main_ref)
+
+        refTab = RefTab()
+        bookmarkTab = BookmarkTab()
+
+        supportFragmentManager.beginTransaction().add(refTabcontent, refTab).commit()
+        //시작할때 내 냉장고탭을 기본으로 보여줌
+
+        tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: Tab?) {
+                val transaction = supportFragmentManager.beginTransaction()
+                when (tab?.text){
+                    "내 냉장고"-> {
+                        transaction.replace(refTabcontent, refTab)
+                    }
+                    "북마크"->transaction.replace(refTabcontent, bookmarkTab)
+                }
+                transaction.commit()
+            }
+
+            override fun onTabReselected(tab: Tab?) {
+                val transaction = supportFragmentManager.beginTransaction()
+                when (tab?.text){
+                    "내 냉장고"-> {
+                        transaction.replace(refTabcontent, refTab)
+                    }
+                    "북마크"->transaction.replace(refTabcontent, bookmarkTab)
+                }
+                transaction.commit()
+            }
+
+            override fun onTabUnselected(tab: Tab?) {
+                val transaction = supportFragmentManager.beginTransaction()
+                when (tab?.text){
+                    "내 냉장고"-> {
+                        transaction.replace(refTabcontent, refTab)
+                    }
+                    "북마크"->transaction.replace(refTabcontent, bookmarkTab)
+                }
+                transaction.commit()
+            }
+        })
+
+        plus.setOnClickListener {
+            val intent = Intent(this, SelectMainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    fun refresh_frag(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+//        transaction.replace(R.id.tabContent, fragment)
+//        transaction.commit()
+        transaction.detach(fragment)
+            .attach(fragment)
+            .commit()
+    }
+}
---
+++ 모듈연결용/main_dp/src/main/java/com/example/main_dp/SelectActivity/FishEtcActivity.kt
@@ -0,0 +1,129 @@
+package com.example.main_dp.SelectActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import com.example.main_dp.R
+import com.example.main_dp.SaveDate
+import com.example.main_dp.exist_indata
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_fish_etc.*
+import kotlinx.android.synthetic.main.activity_fish_etc.TorefButton
+import kotlinx.android.synthetic.main.activity_main_select.*
+
+class FishEtcActivity : AppCompatActivity() {
+    val db = FirebaseFirestore.getInstance()   // Firestore 인스턴스 선언
+    var data_overlap = listOf<String>("0")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fish_etc)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) //액션바뒤로가기
+
+        ChunguhalButton.setOnClickListener {
+            val data = hashMapOf(
+                "photo" to "fish",
+                "id" to "Chunguhal",
+                "menuname" to "청어알",
+                "display" to "1"
+            )
+            data_overlap = listOf("0")
+            exist_indata().existdata(data, data_overlap, this)
+        }
+        SalmonalButton.setOnClickListener {
+            val data = hashMapOf(
+                "photo" to "fish",
+                "id" to "Salmonal",
+                "menuname" to "연어알",
+                "display" to "1"
+            )
+            data_overlap = listOf("0")
+            exist_indata().existdata(data, data_overlap, this)
+        }
+        MyungranjutButton.setOnClickListener {
+            val data = hashMapOf(
+                "photo" to "fish",
+                "id" to "Myungranjut",
+                "menuname" to "명란젓",
+                "display" to "1"
+            )
+            data_overlap = listOf("0")
+            exist_indata().existdata(data, data_overlap, this)
+        }
+        JunuhjutgalButton.setOnClickListener {
+            val data = hashMapOf(
+                "photo" to "fish",
+                "id" to "Junuhjutgal",
+                "menuname" to "전어젓갈",
+                "display" to "1"
+            )
+            data_overlap = listOf("0")
+            exist_indata().existdata(data, data_overlap, this)
+        }
+        NalchialButton.setOnClickListener {
+            val data = hashMapOf(
+                "photo" to "fish",
+                "id" to "Nalchial",
+                "menuname" to "날치알",
+                "display" to "1"
+            )
+            data_overlap = listOf("0")
+            exist_indata().existdata(data, data_overlap, this)
+        }
+        HaepariButton.setOnClickListener {
+            val data = hashMapOf(
+                "photo" to "fish",
+                "id" to "Haepari",
+                "menuname" to "해파리",
+                "display" to "1"
+            )
+            data_overlap = listOf("0")
+            exist_indata().existdata(data, data_overlap, this)
+        }
+        MidudukButton.setOnClickListener {
+            val data = hashMapOf(
+                "photo" to "fish",
+                "id" to "Miduduk",
+                "menuname" to "미더덕",
+                "display" to "1"
+            )
+            data_overlap = listOf("0")
+            exist_indata().existdata(data, data_overlap, this)
+        }
+        MyungranButton.setOnClickListener {
+            val data = hashMapOf(
+                "photo" to "fish",
+                "id" to "Myungran",
+                "menuname" to "명란",
+                "display" to "1"
+            )
+            data_overlap = listOf("0")
+            exist_indata().existdata(data, data_overlap, this)
+        }
+        HaesamButton.setOnClickListener {
+            val data = hashMapOf(
+                "photo" to "fish",
+                "id" to "Haesam",
+                "menuname" to "해삼",
+                "display" to "1"
+            )
+            data_overlap = listOf("0")
+            exist_indata().existdata(data, data_overlap, this)
+        }
+        TorefButton.setOnClickListener {
+            finish() //선택 액티비티 종료해서 내 냉장고로 돌아갈 수 있도록 함
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item?.itemId){
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
+}
