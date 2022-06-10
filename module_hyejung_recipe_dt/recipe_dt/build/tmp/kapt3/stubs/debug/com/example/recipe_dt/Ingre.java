31c446a0ac6e033b97dda9702aadae7c1a5207f4
HyeJung Go <gpwjd0919@sookmyung.ac.kr>
2022-06-10 18:18:00 +0900

    0610 모듈 연결용

---
+++ module_hyejung_recipe_dt/recipe_dt/build/tmp/kapt3/stubs/debug/com/example/recipe_dt/Ingre.java
@@ -0,0 +1,35 @@
+package com.example.recipe_dt;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0002\b\b\u0018\u00002\u00020\u0001B\u001d\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0003\u0012\u0006\u0010\u0005\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0006R\u0011\u0010\u0005\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0007\u0010\bR\u0011\u0010\u0004\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\bR\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\n\u0010\b\u00a8\u0006\u000b"}, d2 = {"Lcom/example/recipe_dt/Ingre;", "", "photo", "", "name", "id", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V", "getId", "()Ljava/lang/String;", "getName", "getPhoto", "recipe_dt_debug"})
+public final class Ingre {
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String photo = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String name = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String id = null;
+    
+    public Ingre(@org.jetbrains.annotations.NotNull()
+    java.lang.String photo, @org.jetbrains.annotations.NotNull()
+    java.lang.String name, @org.jetbrains.annotations.NotNull()
+    java.lang.String id) {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getPhoto() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getName() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getId() {
+        return null;
+    }
+}
---
+++ module_hyejung_recipe_dt/recipe_dt/build/tmp/kapt3/stubs/debug/com/example/recipe_dt/Menu.java
@@ -0,0 +1,43 @@
+package com.example.recipe_dt;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0002\b\n\u0018\u00002\u00020\u0001B%\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0003\u0012\u0006\u0010\u0005\u001a\u00020\u0003\u0012\u0006\u0010\u0006\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0007R\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\b\u0010\tR\u0011\u0010\u0005\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\n\u0010\tR\u0011\u0010\u0004\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000b\u0010\tR\u0011\u0010\u0006\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\f\u0010\t\u00a8\u0006\r"}, d2 = {"Lcom/example/recipe_dt/Menu;", "", "id", "", "photo", "name", "time", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V", "getId", "()Ljava/lang/String;", "getName", "getPhoto", "getTime", "recipe_dt_debug"})
+public final class Menu {
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String id = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String photo = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String name = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String time = null;
+    
+    public Menu(@org.jetbrains.annotations.NotNull()
+    java.lang.String id, @org.jetbrains.annotations.NotNull()
+    java.lang.String photo, @org.jetbrains.annotations.NotNull()
+    java.lang.String name, @org.jetbrains.annotations.NotNull()
+    java.lang.String time) {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getId() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getPhoto() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getName() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getTime() {
+        return null;
+    }
+}
